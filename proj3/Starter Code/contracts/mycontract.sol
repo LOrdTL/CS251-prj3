eeea447d27095a0a3ec43011fd2f498d4d5ad217
LOrdTL <124559861+LOrdTL@users.noreply.github.com>
2025-02-25 12:10:34 +0700

    Update mycontract.sol

--- proj3/Starter Code/contracts/mycontract.sol
+++ proj3/Starter Code/contracts/mycontract.sol
@@ -17,15 +17,34 @@ contract Splitwise {
     function lookup(address debtor, address creditor) public view returns (uint32) {
         return debts[debtor][creditor];
     }
+    function resolveCycle(address debtor, address creditor) internal {
+    address[] memory cycle = findDebtCycle(debtor, creditor);
+    
+    if (cycle.length > 0) {
+        uint32 minDebt = type(uint32).max;
+        
+        // Find the minimum debt in the cycle
+        for (uint i = 0; i < cycle.length - 1; i++) {
+            uint32 debt = debts[cycle[i]][cycle[i + 1]];
+            if (debt < minDebt) {
+                minDebt = debt;
+            }
+        }
+
+        // Reduce debts along the cycle
+        for (uint i = 0; i < cycle.length - 1; i++) {
+            debts[cycle[i]][cycle[i + 1]] -= minDebt;
+        }
+    }
+}
 
     function add_IOU(address creditor, uint32 amount) public {
         require(msg.sender != creditor, "You cannot owe yourself.");
         require(amount > 0, "Amount must be positive.");
-    
         debts[msg.sender][creditor] += amount;
         users[msg.sender] = true;
         users[creditor] = true;
-
+        resolveCycle(msg.sender, creditor);
         emit IOUAdded(msg.sender, creditor, amount);
     }
 
